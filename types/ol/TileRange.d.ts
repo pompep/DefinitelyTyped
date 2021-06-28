33e5bce188a023a47670e4aa6e522332e7e56732
Patrik Pompe <pompep@users.noreply.github.com>
2021-06-28 10:28:36 +0200

    TileRange public properties

--- types/ol/TileRange.d.ts
+++ types/ol/TileRange.d.ts
@@ -2,7 +2,7 @@ import { Size } from './size';
 import { TileCoord } from './tilecoord';
 
 export default class TileRange {
-    constructor(minX: number, maxX: number, minY: number, maxY: number);
+    constructor(public minX: number, public maxX: number, public minY: number, public maxY: number);
     contains(tileCoord: TileCoord): boolean;
     containsTileRange(tileRange: TileRange): boolean;
     containsXY(x: number, y: number): boolean;
